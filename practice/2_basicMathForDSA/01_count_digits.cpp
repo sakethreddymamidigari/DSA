6eef59fd8b05335eb9782814d6ad83021120ec2f
sakethreddymamidigari <sakethreddymamidigari@gmail.com>
2026-03-13 10:50:21 +0530

    chore: reorder floders with numeric prefix

---
+++ practice/2_basicMathForDSA/01_count_digits.cpp
@@ -0,0 +1,28 @@
+
+        count++;
+        n=n/10;
+     //ie the no.of digits in n =int(log10(n))+1
+     //value of log10(n)=no.of digits in n-(0.something)
+    }
+    // printing the number of digits 
+    // runs for log10(n) times
+    //present in the given number
+    cin>>n;
+    countDigits2(n);
+    cout<<count;
+    cout<<count;
+    int count=0;
+    int count=int(log10(n))+1;
+    int n;
+    while (n>0){
+}
+}
+}
+// bruteforce
+// edge case approach
+// runs for 1 time
+#include<bits/stdc++.h>
+int main(){
+using namespace std;
+void countDigits1(int n){
+void countDigits2(int n){
