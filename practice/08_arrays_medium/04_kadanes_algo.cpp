a725da54c70404bd024d630659a0f0b82ef1e72f
sakethreddymamidigari <sakethreddymamidigari@gmail.com>
2026-04-01 17:13:25 +0530

    feat: add kadane's algo

---
+++ practice/08_arrays_medium/04_kadanes_algo.cpp
@@ -0,0 +1,43 @@
+// max sum of sub array sum using "KADANE'S ALGO"
+#include<bits/stdc++.h>
+using namespace std;
+tuple<int,int,int> maxSumArray1(vector<int> arr, int n){
+    int maxSum=INT_MIN,left=0,right=0;
+    for(int i=0;i<n;i++){
+        int sum=0,start=i;
+        for(int j=i;j<n;j++){
+            sum+=arr[j];
+            if(sum>maxSum){
+                left=start;
+                right=j;
+                maxSum=sum;
+            }
+        }
+    }
+    return {left,right,maxSum};
+}
+
+tuple<int,int,int> maxSumArray2(vector<int> arr, int n){
+    int sum=0,maxSum=INT_MIN,start,left=0,right=0;
+    for(int i=0;i<n;i++){
+        if(sum==0) start=i;
+        sum+=arr[i];
+        if(sum>maxSum){
+            maxSum=sum;
+            left=start;
+            right=i;
+        }
+        if(sum<0) sum=0;
+    }
+    return {left,right,maxSum};
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    auto[left,right,maxSum]=maxSumArray2(arr,n);
+    cout<< left<<" "<<right<<","<<maxSum;
+
+}
