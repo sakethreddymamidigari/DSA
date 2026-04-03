3513aab56345eec797a5c49e431ccb1d89f8087b
sakethreddymamidigari <sakethreddymamidigari@gmail.com>
2026-04-03 19:25:50 +0530

    feat: add next permutation

---
+++ practice/08_arrays_medium/07_next_permutation.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void nextPermutation(vector<int> &nums, int n){
+    int ind=-1;
+    for(int i=n-2;i>=0;i--){
+        if(nums[i]<nums[i+1]){
+            ind=i;
+            break;
+        }
+    }
+
+    if(ind==-1){
+        reverse(nums.begin(),nums.end());
+        return;
+    }
+    else{
+        for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin()+ind+1,nums.end());
+    }
+    return;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++) cin>>nums[i];
+    nextPermutation(nums,n);
+    for(int i=0;i<n;i++) cout<<nums[i]<<" ";
+}
