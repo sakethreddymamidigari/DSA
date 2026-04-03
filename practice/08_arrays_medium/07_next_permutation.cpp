6a5deeed94efc50cc6af2b331222a09d9f631163
sakethreddymamidigari <sakethreddymamidigari@gmail.com>
2026-04-03 19:32:25 +0530

    fix: add comment lines in next permutations

--- practice/08_arrays_medium/07_next_permutation.cpp
+++ practice/08_arrays_medium/07_next_permutation.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 void nextPermutation(vector<int> &nums, int n){
+    // TC: O(2*n)
+    // SC: O(1)
     int ind=-1;
+    // checking the pivot index
     for(int i=n-2;i>=0;i--){
         if(nums[i]<nums[i+1]){
             ind=i;
@@ -10,17 +13,20 @@ void nextPermutation(vector<int> &nums, int n){
         }
     }
 
+    // if pivot not found then the reverse the nums
     if(ind==-1){
         reverse(nums.begin(),nums.end());
         return;
     }
     else{
+        // else swap if nums[pivot] with smallest value such that the value>nums[pivot]
         for(int i=n-1;i>ind;i--){
             if(nums[i]>nums[ind]){
                 swap(nums[i],nums[ind]);
                 break;
             }
         }
+        // reversing the all element after pivot index which are in reverse sorted order after swap
         reverse(nums.begin()+ind+1,nums.end());
     }
     return;
