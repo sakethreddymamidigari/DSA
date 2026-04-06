fea9fc836ac81ab28f6f194bf0d78d04ade597fb
sakethreddymamidigari <sakethreddymamidigari@gmail.com>
2026-04-06 16:17:25 +0530

    fix: set zeroes

--- practice/08_arrays_medium/10_set_zero_matrix.cpp
+++ practice/08_arrays_medium/10_set_zero_matrix.cpp
@@ -74,11 +74,13 @@ void setMatrixzeroes3(vector<vector<int>> &matrix,int n, int m){
     }
 
     for(int i=1;i<n;i++){
-        for(int j=1;j<n;j++){
-            if(matrix[0][j]==0 || matrix[i][0]==0) matrix[i][j]=0;
+        for(int j=1;j<m;j++){
+            if(matrix[0][j]==0 || matrix[i][0]==0){
+                matrix[i][j]=0;
+            }
         }
     }
-    if (matrix[0][0]==0) for(int j=0;j<n;j++) matrix[0][j]=0;
+    if (matrix[0][0]==0) for(int j=0;j<m;j++) matrix[0][j]=0;
     if(col==0) for(int i=0;i<n;i++) matrix[i][0]=0;
 
     return;
