78a272fe8b79734096542e9c37c580a2f2db25be
sakethreddymamidigari <sakethreddymamidigari@gmail.com>
2026-04-22 14:06:06 +0530

    modify subset sum equals to k

--- practice/13_dynamic_programming/11_subset_sum_equals_to_k.cpp
+++ practice/13_dynamic_programming/11_subset_sum_equals_to_k.cpp
@@ -19,7 +19,7 @@ bool subsetSum2(int n,int target,vector<int> &nums){
     // SC: O(n*target)
     vector<vector<bool>> dp(n,vector<bool> (target+1,false));
     for(int i=0;i<n;i++) dp[i][0]=true;
-    if(nums[0]==target) dp[0][nums[0]]=true;
+    if(nums[0]<=target) dp[0][nums[0]]=true;
     for(int i=1;i<n;i++){
         for(int t=1;t<=target;t++){
             bool notTake=dp[i-1][t];
